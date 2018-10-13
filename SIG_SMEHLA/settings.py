ca0f2ac21997ae6ac00ffc68594c7e8b95cc27d1
Candido Armando <candido@gmail.com>
2018-10-13 14:11:31 -0400

    creando el modelo Cliente en la aplicacion de servicios

--- SIG_SMEHLA/settings.py
+++ SIG_SMEHLA/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 LOGIN_URL = 'accounts:login'
 DASHBOARD_URL = 'dashboard'
 
--- services/models.py
+++ services/models.py
@@ -1,3 +1,34 @@
+import os
+
 from django.db import models
 
-# Create your models here.
+from SIG_SMEHLA import settings
+
+CIVIL_STATUS = (
+    ('c', 'Casado'),
+    ('v', 'Viudo'),
+    ('s', 'Soltero'),
+)
+
+
+class Client(models.Model):
+    def upload_file(self, filename):
+        return settings.MEDIA_URL + self.ci + os.sep + filename
+
+    ci = models.CharField(max_length=13)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    born_date = models.DateField()
+    civil_status = models.CharField(choices=CIVIL_STATUS, max_length=1)
+    naturalness = models.CharField('Naturalidad', max_length=30)
+    nationality = models.CharField('Nacionalidad', max_length=30)
+    father = models.CharField(max_length=200)
+    mother = models.CharField(max_length=200)
+    address = models.TextField()
+    email = models.EmailField()
+    phone = models.PositiveIntegerField('Numero de telefono')
+    data_attachment = models.FileField('Ficheros adjuntos', upload_to=upload_file)
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
