f455bd27c383b8a4e43ee38c10f0bc7dafa0b3b0
Candido Armando <candido@gmail.com>
2018-10-15 20:59:04 -0400

    creando la vista de inicio en la aplicacion services

--- SIG_SMEHLA/urls.py
+++ SIG_SMEHLA/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
+    path('', include('services.urls')),
     path('admin/', admin.site.urls),
 ]
