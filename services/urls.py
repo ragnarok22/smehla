ad774281c20f32224c561eab61383ca39c7b783d
Candido Armando <candido@gmail.com>
2018-10-17 13:22:33 -0400

    añadiendo la vista, url y template de los servicios

--- services/urls.py
+++ services/urls.py
@@ -4,5 +4,6 @@ from services import views
 
 app_name = 'services'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('services/', views.ServicesView.as_view(), name='services'),
 ]
--- services/views.py
+++ services/views.py
@@ -3,3 +3,7 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'services/index.html'
+
+
+class ServicesView(generic.TemplateView):
+    template_name = 'services/services.html'
