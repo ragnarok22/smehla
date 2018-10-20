e7f9f7fa7e6390dc2f43d88a8511a297558c0751
Candido Armando <candido@gmail.com>
2018-10-20 14:40:58 -0400

    creado las vistas, url y templates para hacer CRUD al cliente

--- services/urls.py
+++ services/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('services/', views.ServicesView.as_view(), name='services'),
     path('tools/', views.ServiceToolsView.as_view(), name='tools'),
+
+    path('client/add/', views.ClientCreateView.as_view(), name='client_create'),
+    path('client/list/', views.ClientListView.as_view(), name='client_list'),
+    path('client/<int:pk>/detail/', views.ClientDetailView.as_view(), name='client_detail'),
+    path('client/<int:pk>/update/', views.ClientUpdateView.as_view(), name='client_update'),
+    path('client/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client_delete'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,4 +1,8 @@
+from django.urls import reverse_lazy
+from django.views import generic
+
 from accounts import mixins
+from services.models import Client
 
 
 class IndexView(mixins.NavbarMixin):
@@ -14,3 +18,32 @@ class ServicesView(mixins.NavbarMixin):
 class ServiceToolsView(mixins.LoginRequiredMixin, mixins.NavbarMixin):
     tab_name = 'tools'
     template_name = 'services/tools.html'
+
+
+class ClientCreateView(mixins.LoginRequiredMixin, generic.CreateView):
+    model = Client
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('services:client_detail', kwargs={'pk': self.object.pk})
+
+
+class ClientDetailView(mixins.LoginRequiredMixin, generic.DetailView):
+    model = Client
+
+
+class ClientListView(mixins.LoginRequiredMixin, generic.ListView):
+    model = Client
+
+
+class ClientUpdateView(mixins.LoginRequiredMixin, generic.UpdateView):
+    model = Client
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('services:client_detail', kwargs={'pk': self.object.pk})
+
+
+class ClientDeleteView(mixins.LoginRequiredMixin, generic.DeleteView):
+    model = Client
+    success_url = reverse_lazy('services:client_list')
