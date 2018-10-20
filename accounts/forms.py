7ffc58e4c7c4ec1f58c80dcbb68a211190f4bf97
Candido Armando <candido@gmail.com>
2018-10-20 14:04:34 -0400

    creando formulario para el login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,21 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
 
 from accounts.models import Profile
+
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(
+        widget=forms.TextInput(
+            attrs={'autofocus': True, 'class': 'form-control'}
+        )
+    )
+    password = forms.CharField(
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control'}
+        )
+    )
 
 
 class ProfileUpdateForm(forms.ModelForm):
