4061fea86f561fac5c9e138111ace51ec87703db
Candido Armando <candido@gmail.com>
2018-10-20 13:27:35 -0400

    reordernando las opciones de algunos modelos de servicios

--- services/models.py
+++ services/models.py
@@ -1,42 +1,14 @@
 from django.db import models
 
-CIVIL_STATUS = (
-    ('s', 'Soltero'),
-    ('o', 'Comprometido'),
-    ('c', 'Casado'),
-    ('v', 'Viudo'),
-)
-
-REQUEST_SPECIFICATION = (
-    ('VT', ''),
-    ('VPT', ''),
-    ('VP', ''),
-    ('VE', ''),
-    ('VTM', ''),
-    ('VOR', ''),
-    ('VTU', ''),
-    ('VCD', ''),
-)
-
-REQUEST_TYPE = (
-    ('C', 'Caducidad'),
-    ('E', 'Extravio'),
-)
-
-SERVICE_STATUS = (
-    ('1', 'Solicitud'),
-    ('2', 'Facturacion'),
-    ('3', 'Revision'),
-    ('4', 'Autenticar'),
-    ('5', 'Entrega'),
-)
-
-SERVICE_TYPE = {
-    'visa': 'Visa',
-}
-
 
 class Client(models.Model):
+    CIVIL_STATUS = (
+        ('s', 'Soltero'),
+        ('e', 'Comprometido'),
+        ('m', 'Casado'),
+        ('w', 'Viudo'),
+    )
+
     def upload_file(self, filename):
         ext = filename.split('.')[-1]
         return 'clients/{}.{}'.format(self.ci, ext)
@@ -53,7 +25,7 @@ class Client(models.Model):
     address = models.TextField()
     email = models.EmailField()
     phone = models.PositiveIntegerField('Numero de telefono')
-    data_attachment = models.FileField('Ficheros adjuntos', upload_to=upload_file)
+    data_attachment = models.FileField('Ficheros adjuntos', upload_to=upload_file, null=True, blank=True)
 
     def get_full_name(self):
         full_name = '%s %s' % (self.first_name, self.last_name)
@@ -61,6 +33,9 @@ class Client(models.Model):
 
 
 class Service(models.Model):
+    SERVICE_TYPE = {
+        'visa': 'Visa',
+    }
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     service_type = None
 
@@ -72,7 +47,29 @@ class Service(models.Model):
 
 
 class Visa(Service):
-    service_type = SERVICE_TYPE['visa']
+    REQUEST_SPECIFICATION = (
+        ('VT', ''),
+        ('VPT', ''),
+        ('VP', ''),
+        ('VE', ''),
+        ('VTM', ''),
+        ('VOR', ''),
+        ('VTU', ''),
+        ('VCD', 'Visado de corta duracion'),
+    )
+    REQUEST_TYPE = (
+        ('C', 'Caducidad'),
+        ('E', 'Extravio'),
+    )
+
+    SERVICE_STATUS = (
+        ('1', 'Solicitud'),
+        ('2', 'Facturacion'),
+        ('3', 'Revision'),
+        ('4', 'Autenticar'),
+        ('5', 'Entrega'),
+    )
+    service_type = Service.SERVICE_TYPE['visa']
     specification = models.CharField('especificacion', max_length=3, choices=REQUEST_SPECIFICATION)
     extension_request_date = models.DateField('Pedido de prorroga')
     request_type = models.CharField('Tipo de solicitud', max_length=1, choices=REQUEST_TYPE)
