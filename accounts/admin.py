b92cfb40fe0470707a3f033b25756c1e4423346d
Candido Armando <candido@gmail.com>
2018-11-04 18:34:25 -0500

    añadiendo datos para poder editar en la vista de administrador al modelo Profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from accounts.models import Profile
 
-admin.site.register(Profile, UserAdmin)
+
+class ProfileAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('occupation',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        ('Others dates', {'fields': ('born_date', 'occupation')}),
+    )
+    list_filter = UserAdmin.list_filter + ('occupation',)
+
+
+admin.site.register(Profile, ProfileAdmin)
