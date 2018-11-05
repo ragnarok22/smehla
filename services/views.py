2fdf8f1ad23e628cbf21a992279bbbf5f6a793c6
Candido Armando <candido@gmail.com>
2018-11-05 17:07:18 -0500

    arreglando problemas de traduccion en el buscar estado de peticion de servicio

--- services/views.py
+++ services/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from django.views import generic
 
 from accounts import mixins
