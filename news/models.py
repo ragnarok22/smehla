6153c8d4e57d48498e634f651a43c21e0c8d74a0
Candido Armando <candido@gmail.com>
2018-11-21 21:50:35 -0500

    añadiendo imagen a las noticias

--- news/models.py
+++ news/models.py
@@ -3,6 +3,11 @@ from django.utils.translation import gettext_lazy as _
 
 
 class News(models.Model):
+    def picture_news(self, filename):
+        ext = filename.split('.')[-1]
+        return 'news/{}/{}.{}'.format(self.pub_date, self.title, ext)
+
+    picture = models.ImageField(_('Picture'), upload_to=picture_news)
     title = models.CharField(_('Title'), max_length=100)
     content = models.TextField(_('Content'))
     pub_date = models.DateField(_('Publication date'), auto_now=True)
