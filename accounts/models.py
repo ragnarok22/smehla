1baf3ed308e292e547557b9db3c884e520cfe77f
Candido Armando <candido@gmail.com>
2019-01-18 19:53:13 -0500

    añadiendo un nuevo cargo a los usuarios

--- accounts/models.py
+++ accounts/models.py
@@ -19,6 +19,7 @@ class CustomUserManager(UserManager):
 class Profile(AbstractUser):
     OCCUPATION_TYPE = (
         ('FAC', _('Functionary of Attention to Customer')),
+        ('FUF', _('Functionary of Finance')),
         ('BAC', _('Boss of Attention to Customer')),
         ('BDAC', _('Boss of Departament of Attention to Customer')),
         ('DIR', _('Director')),
